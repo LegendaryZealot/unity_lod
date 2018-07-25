e7382e3f822280c51817b22c41b2dd6e27664773
LegendaryZealot <875626439@qq.com>
2018-07-25 10:46:16 +0800

    first commit

---
+++ Assets/Scripts/ChangPostion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangPostion : MonoBehaviour {
+
+	[SerializeField]
+	Slider slider;
+	
+	void Update () {
+		this.transform.position=new Vector3(0,0,-slider.value);
+	}
+}
